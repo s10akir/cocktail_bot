1481a6109c8269f16656333fc68866987412ca08
Akira Shinohara <k017c1067@it-neec.jp>
2019-06-25 16:08:03 +0900

    [add] merge times_user to times_all function

--- index.js
+++ index.js
@@ -3,7 +3,53 @@
 
 require('dotenv').config();
 
+const { WebClient } = require('@slack/web-api');
+const { RTMClient } = require('@slack/rtm-api');
 
 const SLACK_TOKEN = process.env.SLACK_TOKEN;
 const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
 
+
+const web = new WebClient(SLACK_TOKEN);
+const rtm = new RTMClient(SLACK_BOT_TOKEN);
+
+
+/**
+ * 分報を一つのチャンネルにまとめる
+ * @params event RTMのイベント
+ */
+const mergeDiary = async event => {
+  // テキストの含まれない投稿とスレッドは無視
+  if (!event.text || event.thread_ts) return;
+
+  const res = await web.users.info({
+    user: event.user,
+  });
+  const user = res.user;
+
+  // 人によってはdisplay_nameを設定していないのでその場合はreal_nameとする
+  const username = user.profile.display_name ? user.display_name : user.profile.real_name;
+  const icon_url = user.profile.image_48;
+
+  await web.chat.postMessage({
+    channel: 'times_all',
+    // TODO: event.textだけでは情報が足りないのでそのうちなんとかする
+    // e.g. 画像やスタンプなど
+    text: event.text,
+    username,
+    icon_url,
+  });
+};
+
+
+// このBotが参加しているチャンネルの全メッセージイベントを受け取る
+rtm.on('message', event => {
+  console.log(event);
+  if (event.channel !== 'CKX9TB2DC') {
+    mergeDiary(event);
+  }
+});
+
+(async () => {
+  await rtm.start();
+})();
